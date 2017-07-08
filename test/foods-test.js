baf62ce345825efffb90a722b2eefe38f4f4d6a0
stovermc <stovermc@gmail.com>
2017-07-08 11:46:07 -0600

    Add form for adding a new food
    * added functions to post new food to the database
    
    * new food is appended to table
    
    * test is passing but need to refactor before/after action to clear api
    so that it is more test reliable.

--- test/foods-test.js
+++ test/foods-test.js
@@ -2,7 +2,8 @@ const assert    = require('chai').assert;
 const webdriver = require('selenium-webdriver');
 const until     = webdriver.until;
 const test      = require('selenium-webdriver/testing');
-const rootPath = "http://localhost:8080"
+const rootPath  = "http://localhost:8080"
+const host      = require('../lib/config').host
 
 test.describe('visit foods.html', function () {
   let driver
@@ -12,22 +13,47 @@ test.describe('visit foods.html', function () {
     driver = new webdriver.Builder()
       .forBrowser("chrome")
       .build()
+
+    console.log(host)
+    driver.post(`${host}/api/v1/foods`,
+      { name: 'pie', calories: 250 } )
+    // const testFood = new Food({name: 'pie', calories: 150})
+    // testFood.create()
+    // .then(function(fullFood){
+    //   $('#foods-table').append(fullFood.toHTML())
+    // })
   })
 
   test.afterEach(function() {
-  driver.quit();
+    driver.quit()
   })
 
   test.it('should display table of all my foods', function () {
-    // If I visit foods.html,
     driver.get(`${rootPath}/foods.html`)
     driver.wait(until.elementLocated({css: "#foods-table .food"}))
     driver.findElements({css: "#foods-table .food"})
     .then(function(foods){
       assert.lengthOf(foods, 9)
     })
-    // I should see a table of all my foods,
-    // with Name, Calories and a delete icon for each food
+  })
 
+  test.it('should be able to fill in form with name and calories', function () {
+    //   When I visit foods.html,
+    driver.get(`${rootPath}/foods.html`)
+
+    // I can enter a name and calorie amount,
+    // and create a new food by clicking "Add Food"
+    driver.findElement({css: "input[name='food-name']"})
+      .sendKeys("Scramble")
+    driver.findElement({css: "input[name='food-calories']"})
+      .sendKeys("300")
+    driver.findElement({css: "input[name='add-food-button']"})
+      .click()
+
+    driver.wait(until.elementLocated({css: "tr[data-id='10']"}))
+    driver.findElements({css: "#foods-table .food"})
+    .then(function(foods){
+      assert.lengthOf(foods, 10)
+    })
   })
 })
