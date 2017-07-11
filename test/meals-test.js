ad86e4cc3b3718f62cf88a9eb52a26506054f91b
stovermc <stovermc@gmail.com>
2017-07-11 08:03:34 -0600

    Add test for foods in meal tables
    * tests passing
    
    * need to add snacks to seed file in order to pass skipped test for
    snacks

--- test/meals-test.js
+++ test/meals-test.js
@@ -4,7 +4,7 @@ const until     = webdriver.until;
 const test      = require('selenium-webdriver/testing');
 const rootPath  = "http://localhost:8080"
 
-test.describe.skip('visit index.html', function () {
+test.describe('visit index.html', function () {
   let driver
   this.timeout(10000)
 
@@ -26,6 +26,51 @@ test.describe.skip('visit index.html', function () {
     driver.findElements({css: ".meal"})
     .then(function(meals){
       assert.lengthOf(meals, 4)
+    })
+  })
+
+  test.it('should display foods for breakfast', function () {
+    driver.get(`${rootPath}/index.html`)
+
+    driver.wait(until.elementsLocated({css: "#breakfast"}))
+
+    driver.findElements({css: "#breakfast .name"})
+    .then(function(foods){
+      assert.lengthOf(foods, 2)
+    })
+  })
+
+  test.it('should display foods for lunch', function () {
+    driver.get(`${rootPath}/index.html`)
+
+    driver.wait(until.elementsLocated({css: "#lunch"}))
+
+    driver.findElements({css: "#lunch .name"})
+    .then(function(foods){
+      assert.lengthOf(foods, 2)
+    })
+  })
+
+  test.it('should display foods for dinner', function () {
+    driver.get(`${rootPath}/index.html`)
+
+    driver.wait(until.elementsLocated({css: "#dinner"}))
+
+    driver.findElements({css: "#dinner .name"})
+    .then(function(foods){
+      assert.lengthOf(foods, 3)
+    })
+  })
+
+  //need to add snacks to seed file in order to pass
+  test.it.skip('should display foods for snack', function () {
+    driver.get(`${rootPath}/index.html`)
+
+    driver.wait(until.elementsLocated({css: "#snack"}))
+
+    driver.findElements({css: "#snack .name"})
+    .then(function(foods){
+      assert.lengthOf(foods, 2)
     })
   })
 
